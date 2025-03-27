f6a2adc726741e7d53f1228d81f1ff1c40e11783
Lucas Botinelly <lucas_botinelly@hotmail.com>
2025-03-26 21:05:11 -0400

    fix: updating deploy

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from src.api.routes.user import router
+import uvicorn
+import os
+
+HOST = os.getenv("APP_HOST", "0.0.0.0")
+PORT = int(os.getenv("APP_PORT", 8000))
 
 app = FastAPI()
 app.include_router(router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host=HOST, port=PORT, reload=True)
